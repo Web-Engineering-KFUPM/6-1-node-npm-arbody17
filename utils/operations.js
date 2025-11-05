baf0802759504b79e5456a453816bfd325e6120c
arbody <arbody.arb@gmail.com>
2025-11-05 21:42:19 +0300

    Done the lab

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,21 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  return numbers.slice(1).reduce((diff, num) => diff - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((product, num) => product * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  return numbers.slice(1).reduce((quotient, num) => {
+    if (num === 0) {
+      console.log("Error: Division by zero");
+      process.exit(1);
+    }
+    return quotient / num;
+  }, numbers[0]);
 }
-
